b071e588d77549ac176952cb87c75d15a1f2600e
Catalina Laverde <catalina@spotify.com>
2024-05-26 17:27:56 -0400

    Implement challenge

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -3,7 +3,9 @@ package com.linkedin.collections;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class RoomService {
@@ -13,49 +15,51 @@ public class RoomService {
 	public RoomService() {
 		this.inventory = new LinkedHashSet<>();
 	}
-	
+
 	public void applyDiscount(final double discount) {
-		
-		//Reduces the rate of each room by the provided discount
-	
+		// This is how you can modify collections while iterating through them
+		inventory.iterator().forEachRemaining(r -> {
+			double newRate = r.getRate() - (r.getRate() * discount);
+			r.setRate(newRate);
+		});
 	}
 
 	public Collection<Room> getRoomsByCapacity(final int requiredCapacity) {
-		
-		//Returns a new collection of rooms that meet or exceed the provided capacity
-		
-		return null;
-		
+		// Returns a new collection of rooms that meet or exceed the provided capacity
+		return inventory.stream()
+				.filter(r -> r.getCapacity() >= requiredCapacity)
+				.collect(Collectors.toCollection(LinkedHashSet::new));
 	}
-	
-	public Collection<Room> getRoomByRateAndType(final double rate, final String type){
-	
-		//Returns a new collection of rooms with a rate below the provided rate and that match the provided type
-		
-		return null;
-		
+
+	public Collection<Room> getRoomByRateAndType(final double rate, final String type) {
+		// Returns a new collection of rooms with a rate below the provided rate and
+		// that match the provided type
+		return inventory.stream()
+				.filter(r -> (r.getRate() < rate && r.getType().equals(type)))
+				.collect(Collectors.toCollection(LinkedHashSet::new));
+
 	}
-	
+
 	public boolean hasRoom(Room room) {
-		
-		return this.inventory.contains(room); 
+
+		return this.inventory.contains(room);
 	}
-	
+
 	public Room[] asArray() {
-		
+
 		return this.inventory.toArray(new Room[0]);
 	}
-	
-	public Collection<Room> getByType(String type){
+
+	public Collection<Room> getByType(String type) {
 
 		Collection<Room> copy = new HashSet<>(this.inventory);
 		copy.removeIf(r -> !r.getType().equals(type));
 		return copy;
-		
+
 	}
 
 	public Collection<Room> getInventory() {
-		return new HashSet<>(this.inventory); 
+		return new HashSet<>(this.inventory);
 	}
 
 	public void createRoom(String name, String type, int capacity, double price) {
