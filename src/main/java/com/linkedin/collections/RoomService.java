4f518896ed4db583e4313e87db034fa9005a02fa
Catalina Laverde <catalina@spotify.com>
2024-05-26 15:37:24 -0400

    Challenge 1

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -1,40 +1,31 @@
 package com.linkedin.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class RoomService {
 
-	// 1. Declare a Collection to store Room Inventory
+	private Collection<Room> rooms;
 
 	public RoomService() {
-		
-	// 2. Initialize Collection and assign it to the Room Inventory
-
+		rooms = new ArrayList<Room>();
 	}
 
-	public Collection<Room> getInventory(){
-		
-	// 3. Return the Room Inventory
-		
-		return null;
+	public Collection<Room> getInventory() {
+		return rooms;
 	}
-	
+
 	public void createRoom(String name, String type, int capacity, double rate) {
-	
-	// 4. Add a new Room to the Room Inventory using the provided parameters
-
+		rooms.add(new Room(name, type, capacity, rate));
 	}
 
 	public void createRooms(Room[] rooms) {
-	
-	// 5. Add the Rooms provided in the Array to the Room Inventory
-
+		this.rooms.addAll(Arrays.asList(rooms));
 	}
-	
+
 	public void removeRoom(Room room) {
-		
-	// 6. Remove the provided Room from the Room Inventory
-
+		this.rooms.remove(room);
 	}
-
 }
